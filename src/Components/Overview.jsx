896655f9823dba7235554eff4a46107f22ce2011
maruf-ahmed <marufahamed1110@gmail.com>
2024-08-14 23:56:15 +0600

    solving

--- src/Components/Overview.jsx
+++ src/Components/Overview.jsx
@@ -173,7 +173,7 @@ function Overview() {
           </div>
         </div>
         <div className="col-span-5  mr-6">
-          <div className="flex justify-between items-start">
+          <div className="flex justify-between gap-6 items-start">
             <div className=" flex bg-[#F7F7FF] p-8 rounded-2xl gap-12">
               <div className=" space-y-6">
                 <h1 className="text-5xl font-bold">04</h1>
