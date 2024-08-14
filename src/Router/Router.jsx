960103f4aea1561b15b05f4ad137989735d73dad
maruf-ahmed <marufahamed1110@gmail.com>
2024-08-14 23:47:48 +0600

    problem solving

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,19 +1,15 @@
-import {
-    createBrowserRouter,
-   
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "../Layouts/Dashboard";
 import Overview from "../Components/Overview";
 export const router = createBrowserRouter([
-    {
-      path: "/",
-        element: <Dashboard />,
-        children: [
-            {
-                path: '/overview',
-                element:<Overview/>
-            }
-        ]
-    },
-
-  ]);
+  {
+    path: "/overview",
+    element: <Dashboard />,
+    children: [
+      {
+        path: "/overview",
+        element: <Overview />,
+      },
+    ],
+  },
+]);
